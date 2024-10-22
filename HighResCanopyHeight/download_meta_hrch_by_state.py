d54cd13ee7db55999adb2b24339352396b3f2315
Zack Loken <zack@savannainstitute.org>
2024-10-22 17:13:45 -0500

    Update download_meta_hrch_by_state.py

--- HighResCanopyHeight/download_meta_hrch_by_state.py
+++ HighResCanopyHeight/download_meta_hrch_by_state.py
@@ -4,7 +4,6 @@ import os
 import requests
 import logging
 import multiprocessing
-from multiprocessing import current_process
 from retry import retry
 import time
 
@@ -54,14 +53,14 @@ def check_and_split_county(county_geometry, max_dim, scale, max_request_size):
         x_step = width / num_splits
         y_step = height / num_splits
 
-        # Split the region into smaller subregions if needed
+        # Split the region into smaller subregions if needed (GeoJSON format)
         sub_regions = [
-            ee.Geometry.Rectangle([
-                min_x + i * x_step,
-                min_y + j * y_step,
-                min_x + (i + 1) * x_step,
-                min_y + (j + 1) * y_step
-            ])
+            [
+                min_x + i * x_step, # xmin
+                min_y + j * y_step, # ymin
+                min_x + (i + 1) * x_step, # xmax
+                min_y + (j + 1) * y_step # ymax
+            ]
             for i in range(num_splits)
             for j in range(num_splits)
         ]
@@ -75,7 +74,7 @@ def getRequests(state_name:str):
     """Split counties in a state into subregions and return a list of 
     tuples with the county name followed by its subregions."""
     try:
-        # Load the counties FeatureCollection using the state name to filter
+        # Load the counties for state into feature collection
         counties = ee.FeatureCollection('TIGER/2018/Counties').filter(
             ee.Filter.eq(
                 'STATEFP',
@@ -95,68 +94,102 @@ def getRequests(state_name:str):
                 scale = 1, 
                 max_request_size = 50331648
             )
+            
             # Create a tuple with the county name followed by its subregions
             return (county_name, *sub_regions)
+        
         # Use map to process each county and create the list of subregions
         return list(map(process_county, counties.aggregate_array('NAME').getInfo()))
+    
     except Exception as e:
         logging.error(f'Error processing state {state_name}: {e}')
         return []
 
 
-def getResults(index, county_sub_regions):
+def getResults(index, counties_and_sub_regions):
+    """Prepare sub-region processing tasks for each county"""
+    county_name, *sub_regions = counties_and_sub_regions[index]
+
+    # Prepare arguments for parallel processing
+    tasks = [(county_name, sub_region_idx, sub_region_geojson) for sub_region_idx, sub_region_geojson in enumerate(sub_regions)]
+    
+    return tasks
+
+
+def process_sub_region(county_name, sub_region_idx, sub_region):
     """Download the sub-region images for each county in the list of tuples"""
-    county_name, *sub_regions = county_sub_regions[index]
-    logging.info(f'Processing {county_name}...')
+    # Convert GeoJSON to ee.Geometry.Rectangle
+    sub_region = ee.Geometry.Rectangle(sub_region)
+    
+    # Get the sub-region image
+    sub_region_image = ee.ImageCollection('projects/meta-forest-monitoring-okw37/assets/CanopyHeight').filterBounds(sub_region).mosaic().clip(sub_region)
 
-    # create a directory for the county
-    county_dir = f"C:/Users/exx/Desktop/CRP_Windbreak_ID/meta_chm_data/{state_name}" + '/' + str(county_name)
-    os.makedirs(county_dir, exist_ok=True)
+    max_retries = 5
+    backoff_factor = 1
 
-    start_time = time.time()
+    for attempt in range(max_retries):
+        try:
+            # Get the download URL for the image
+            url = sub_region_image.getDownloadURL({
+                'scale': 1,
+                'format': 'GEO_TIFF'
+            })
 
-    # download image for each sub region in county
-    def download_and_save(sub_region_idx_sub_region):
-        sub_region_idx, sub_region = sub_region_idx_sub_region
-        # Get the sub-region image
-        sub_region_image = ee.ImageCollection('projects/meta-forest-monitoring-okw37/assets/CanopyHeight').filterBounds(sub_region).mosaic().clip(sub_region)
-        # Get the download URL for the image
-        url = sub_region_image.getDownloadURL({
-            'scale': 1,
-            'format': 'GEO_TIFF'
-        })
+            # Download the TIFF from URL
+            r = requests.get(url, stream=True)
+            if r.status_code != 200:
+                r.raise_for_status()
 
-        # Download the TIFF from URL
-        r = requests.get(url, stream=True)
-        if r.status_code != 200:
-            r.raise_for_status()
+            # create a directory for the county
+            county_dir = f"C:/Users/exx/Desktop/CRP_Windbreak_ID/meta_chm_data/{state_name}" + '/' + str(county_name)
+            os.makedirs(county_dir, exist_ok=True)
 
-        # save the image to the county directory with filename: {county_name}_{sub_region_idx}_chm.tif
-        with open(f'{county_dir}/{county_name}_{sub_region_idx}_chm.tif', 'wb') as f:
-            for chunk in r.iter_content(chunk_size=8192):
-                f.write(chunk)
+            # save the image to the county directory with filename: {county_name}_{sub_region_idx}_chm.tif
+            with open(f'{county_dir}/{county_name}_{sub_region_idx}_chm.tif', 'wb') as f:
+                for chunk in r.iter_content(chunk_size=8192):
+                    f.write(chunk)
 
-        logging.info(f'Saved {county_name}_{sub_region_idx}_chm.tif to {county_dir} in {time.time() - start_time:.2f} seconds')
+            logging.info(f'Saved {county_name}_{sub_region_idx}_chm.tif to {county_dir}')
+            break  # Exit the retry loop if successful
 
-    list(map(download_and_save, enumerate(sub_regions)))
+        except requests.exceptions.HTTPError as e:
+            if r.status_code == 429:
+                wait_time = backoff_factor * (2 ** attempt)
+                logging.warning(f"Rate limit exceeded. Retrying in {wait_time} seconds...")
+                time.sleep(wait_time)
+            else:
+                logging.error(f"HTTP error occurred: {e}")
+                break
+        except ee.EEException as e:
+            logging.error(f"Error processing sub_region {sub_region_idx}: {e}")
+            break
+        except Exception as e:
+            logging.error(f"Unexpected error: {e}")
+            break
 
 
-# configure logging and start multiprocessing
 if __name__ == '__main__':
     program_start_time = time.time()
 
     # store counties and subregions in a list
     counties_and_sub_regions = getRequests(state_name)
     
-    # create multiprocessing pool with 48 workers
-    pool = multiprocessing.Pool(num_workers)
+    # Process each county one at a time
+    for index in range(len(counties_and_sub_regions)):
+        print(f'Beginning {counties_and_sub_regions[index][0]} County')
 
-    # use starmap to parallelize the download process
-    pool.starmap(getResults, enumerate(counties_and_sub_regions))
-
-    # close and join the pool to ensure that all processes are completed before exiting
-    pool.close()
-    pool.join()
+        # Get tasks for the current county
+        tasks = getResults(index, counties_and_sub_regions)
+        
+        # Create a multiprocessing pool with num_workers
+        pool = multiprocessing.Pool(num_workers)
+        
+        # Use starmap to parallelize the processing of sub-regions for the current county
+        pool.starmap(process_sub_region, tasks)
+        
+        # Close and join the pool to ensure that all processes are completed before moving to the next county
+        pool.close()
+        pool.join()
 
     program_end_time = time.time()
 
