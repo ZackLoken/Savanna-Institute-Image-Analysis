84aef3bde4047a22233d474562c7fc261dc8766d
ZackLoken <zack@savannainstitute.org>
2024-11-26 10:01:02 -0600

    Update download_NAIP_by_state.py

--- CRP_Windbreak_ID/download_NAIP_by_state.py
+++ CRP_Windbreak_ID/download_NAIP_by_state.py
@@ -35,14 +35,13 @@ logging.basicConfig(
 
 def initialize_ee():
     """Initialize the Earth Engine module."""
-    ee.Initialize(url='https://earthengine-highvolume.googleapis.com', project='ee-zack')
+    ee.Initialize(url='https://earthengine-highvolume.googleapis.com', project='ee-zack-loken')
 
 initialize_ee()
 
 def split_geometry(geometry, max_dim, scale, max_request_size):
     """Check if the geometry exceeds GEE maximum dimensions and 
     split it into smaller subregions if needed."""
-    # Get the bounding box of the geometry
     bbox = geometry.bounds().getInfo()['coordinates'][0]
     min_x, min_y = bbox[0]
     max_x, max_y = bbox[2]
@@ -50,20 +49,13 @@ def split_geometry(geometry, max_dim, scale, max_request_size):
     width = max_x - min_x
     height = max_y - min_y
 
-    # Convert degrees to meters (approx)
     width_m = width * 111319.5
     height_m = height * 111319.5
     
-    # Calculate the number of splits needed
     num_splits = 1
     while (width_m / num_splits) / scale > max_dim or (height_m / num_splits) / scale > max_dim:
         num_splits *= 2
 
-    # Recalculate width and height for the new sub-regions
-    width_m /= num_splits
-    height_m /= num_splits
-
-    # Further split if the request size exceeds the limit
     while (width_m * height_m * 4) > max_request_size:
         num_splits *= 4
 
@@ -71,13 +63,12 @@ def split_geometry(geometry, max_dim, scale, max_request_size):
         x_step = width / num_splits
         y_step = height / num_splits
 
-        # Split the region into smaller subregions (GeoJSON format)
         sub_regions = [
             [
-                min_x + i * x_step, # xmin
-                min_y + j * y_step, # ymin
-                min_x + (i + 1) * x_step, # xmax
-                min_y + (j + 1) * y_step # ymax
+                min_x + i * x_step,
+                min_y + j * y_step,
+                min_x + (i + 1) * x_step,
+                min_y + (j + 1) * y_step
             ]
             for i in range(num_splits)
             for j in range(num_splits)
@@ -87,12 +78,10 @@ def split_geometry(geometry, max_dim, scale, max_request_size):
     else:
         return [geometry]
 
-
 def getRequests(state_name: str, batch_size: int = 10, retries: int = 3, delay: int = 5):
     """Split counties in a state into subregions and return a list of 
     tuples with the county name followed by its subregions."""
     try:
-        # Load the counties for state into feature collection
         counties = ee.FeatureCollection('TIGER/2018/Counties').filter(
             ee.Filter.eq(
                 'STATEFP',
@@ -116,7 +105,6 @@ def getRequests(state_name: str, batch_size: int = 10, retries: int = 3, delay: 
                 while attempt < retries:
                     try:
                         logging.info(f"Loading county: {county_name}")
-                        # Split the county into smaller subregions
                         sub_regions = split_geometry(
                             geometry = counties.filter(ee.Filter.eq('NAME', county_name)).first().geometry(), 
                             max_dim = MAX_DIM, 
@@ -124,20 +112,18 @@ def getRequests(state_name: str, batch_size: int = 10, retries: int = 3, delay: 
                             max_request_size = MAX_REQUEST_SIZE
                         )
                         
-                        # Create a tuple with the county name followed by its subregions
                         batch_results.append((county_name, *sub_regions))
                         break
                     except Exception as e:
                         attempt += 1
-                        logging.error(f"Error processing county {county_name} on attempt {attempt}: {e}")
                         if attempt < retries:
-                            time.sleep(delay * attempt)  # Exponential backoff
+                            time.sleep(delay * attempt)
                         else:
                             logging.error(f"Failed to process county {county_name} after {retries} attempts.")
                             continue
 
             all_results.extend(batch_results)
-            time.sleep(1)  # Add a small delay to avoid rate limiting issues
+            time.sleep(1)
 
         return all_results
     
@@ -145,40 +131,37 @@ def getRequests(state_name: str, batch_size: int = 10, retries: int = 3, delay: 
         logging.error(f'Error processing state {state_name}: {e}')
         return []
 
-
 def getResults(index, counties_and_sub_regions, failed_sub_regions, state_name):
     """Prepare sub-region processing tasks for each county"""
     county_name, *sub_regions = counties_and_sub_regions[index]
 
-    # Prepare arguments for parallel processing
     tasks = [(state_name, county_name, sub_region_idx, sub_region_geojson, failed_sub_regions) for sub_region_idx, sub_region_geojson in enumerate(sub_regions)]
     
     return tasks
 
-
 def process_sub_region(state_name, county_name, sub_region_idx, sub_region, failed_sub_regions):
     """Download the sub-region images for each county in the list of tuples"""
-    max_retries = 5
-    backoff_factor = 1
-
     sub_region = ee.Geometry.Rectangle(sub_region)
 
-    for attempt in range(max_retries):
-        try:
-            sub_region_image = ee.ImageCollection('USDA/NAIP/DOQQ') \
+    sub_region_image = ee.ImageCollection('USDA/NAIP/DOQQ') \
                 .filterBounds(sub_region) \
                 .filter(ee.Filter.calendarRange(2018, 2024, 'year')) \
                 .sort('system:time_start', False) \
                 .first() \
                 .clip(sub_region)
+    
+    max_retries = 5
+    backoff_factor = 1
 
+    for attempt in range(max_retries):
+        try:
             url = sub_region_image.getDownloadURL({
                 'scale': SCALE,
                 'format': 'GEO_TIFF',
                 'bands': ['R', 'G', 'B', 'N']
             })
 
-            r = requests.get(url, stream=True, timeout=60)  # Add timeout to requests
+            r = requests.get(url, stream=True, timeout=60)
             if r.status_code != 200:
                 r.raise_for_status()
 
@@ -192,7 +175,7 @@ def process_sub_region(state_name, county_name, sub_region_idx, sub_region, fail
                     f.write(chunk)
 
             logging.info(f"Saved {filename} to {county_dir}")
-            break  # Exit the retry loop if successful
+            break
 
         except requests.exceptions.HTTPError as e:
             if r.status_code == 429:
@@ -201,65 +184,57 @@ def process_sub_region(state_name, county_name, sub_region_idx, sub_region, fail
                 time.sleep(wait_time)
             else:
                 logging.error(f"HTTP error occurred: {e}")
-                failed_sub_regions.append((county_name, sub_region_idx, sub_region.getInfo()['coordinates']))
+                failed_sub_regions.put((county_name, sub_region_idx, sub_region.getInfo()['coordinates']))
                 break
         except requests.exceptions.RequestException as e:
             logging.error(f"Request error occurred: {e}")
-            failed_sub_regions.append((county_name, sub_region_idx, sub_region.getInfo()['coordinates']))
+            failed_sub_regions.put((county_name, sub_region_idx, sub_region.getInfo()['coordinates']))
             break
         except ee.EEException as e:
             logging.error(f"Error processing sub_region {sub_region_idx}: {e}")
-            failed_sub_regions.append((county_name, sub_region_idx, sub_region.getInfo()['coordinates']))
+            failed_sub_regions.put((county_name, sub_region_idx, sub_region.getInfo()['coordinates']))
             break
         except Exception as e:
             logging.error(f"Unexpected error in sub_region {sub_region_idx}: {e}")
-            failed_sub_regions.append((county_name, sub_region_idx, sub_region.getInfo()['coordinates']))
+            failed_sub_regions.put((county_name, sub_region_idx, sub_region.getInfo()['coordinates']))
             break
 
-    # If the loop completes without a successful download, log the failure
     else:
         logging.error(f"Failed to download sub-region {sub_region_idx} for county {county_name} after {max_retries} attempts")
-        failed_sub_regions.append((county_name, sub_region_idx, sub_region.getInfo()['coordinates']))
-
+        failed_sub_regions.put((county_name, sub_region_idx, sub_region.getInfo()['coordinates']))
 
 if __name__ == '__main__':
-    # Global list to store failed sub-regions
     manager = multiprocessing.Manager()
     failed_sub_regions = manager.list()
 
     program_start_time = time.time()
 
     for state_name in state_names:
-        # store counties and subregions in a list
         counties_and_sub_regions = getRequests(state_name)
         print(f'Found {len(counties_and_sub_regions)} counties in {state_name}')
         
-        # Process each county one at a time
         for index in range(len(counties_and_sub_regions)):
             county_folder = os.path.join(out_dir, state_name, counties_and_sub_regions[index][0])
-            if os.path.exists(county_folder): # handle folders that have already been completed
+            if os.path.exists(county_folder):
                 print(f'Skipping {counties_and_sub_regions[index][0]} County as it already exists.')
                 continue
 
             print(f'Beginning {counties_and_sub_regions[index][0]} County')
             county_start_time = time.time()
 
-            # Get tasks for the current county
             tasks = getResults(index, counties_and_sub_regions, failed_sub_regions, state_name)
             
             # Create a multiprocessing pool with num_workers
             pool = multiprocessing.Pool(num_workers, initializer=initialize_ee)
-            
+
             # Use starmap to parallelize the processing of sub-regions for the current county
             results = pool.starmap_async(process_sub_region, tasks)
-            
-            # Monitor the processing time
+                
             while not results.ready():
-                if time.time() - county_start_time > 1200:  # 20 minutes
+                if time.time() - county_start_time > 1200:
                     logging.warning(f"Processing county {counties_and_sub_regions[index][0]} timed out.")
                     pool.terminate()
                     pool.join()
-                    # Add remaining tasks to failed_sub_regions
                     for task in tasks[results._index:]:
                         failed_sub_regions.append((task[1], task[2], task[3]))
                     break
@@ -269,11 +244,10 @@ if __name__ == '__main__':
                 pool.close()
                 pool.join()
 
-    # Save failed sub-regions to a text file
-    failed_sub_regions_file = f"{out_dir}/failed_sub_regions.txt"
-    with open(failed_sub_regions_file, 'w') as f:
-        for item in failed_sub_regions:
-            f.write(f"{item}\n")
+        failed_sub_regions_file = f"{os.path.join(out_dir, state_name)}/failed_sub_regions.txt"
+        with open(failed_sub_regions_file, 'w') as f:
+            for item in failed_sub_regions:
+                f.write(f"{item}\n")
 
     program_end_time = time.time()
 
